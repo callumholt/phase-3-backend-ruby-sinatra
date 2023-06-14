f6510c8666333af9a4cc17b4bc80b2b2fb8b48d1
callum <callumpholt@gmail.com>
2023-06-14 17:05:00 +1000

    updates

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require 'sinatra/activerecord'
 require 'yaml'
 require 'psych'
 
+# the below 'before' piece of code is to help manage the CORS-POLICY issue
 before do
     headers 'Access-Control-Allow-Origin' => '*',
             'Access-Control-Allow-Methods' => 'OPTIONS, GET, POST, PUT',
@@ -26,13 +27,17 @@ set :database, { adapter: 'sqlite3', database: 'db.sqlite3' }
 class Todo < ActiveRecord::Base
 end
 
-
+# the below was also necessary to help fix the cors-policy
 options "*" do
   response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS,UPDATE"
 
   response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
   halt HTTP_STATUS_OK
 end
+
+# the below was the piece of ruby code to manage the POST request route that was sent to the /todo url
+# it checks to see if the table already exists, and if it does not, it first creates the table using active record
+
 
 post '/todo' do
     begin
@@ -48,6 +53,9 @@ post '/todo' do
         t.string :user
       end
     end
+
+# then it takes the json request and parses it, and sets a range of variables to the parsed data
+# then it takes the variables and creates a new todo with the data
 
     request_body = JSON.parse(json_request_body)
     todo_value = request_body["todo"]
@@ -67,12 +75,15 @@ post '/todo' do
 
 end
 
+# this deals with the GET request for simply returning a specific todo
+
 get '/todo/:user' do
     user = params[:user]
     todos = Todo.select(:id, :todo, :category).where(user: user)
     todos.to_json
   end
 
+# the below is a PUT request that updates a specific todo
 
 put '/todo/:id' do
     begin
@@ -83,7 +94,7 @@ put '/todo/:id' do
   
       if todo.update(todo: params["todo"], category: params["category"], user: params["user"])
         todo.to_json
-        puts "Updated todo: #{todo.to_json}"
+        # puts "Updated todo: #{todo.to_json}"
 
       else
         halt 400, todo.errors.full_messages.join(', ')
@@ -97,6 +108,8 @@ put '/todo/:id' do
 
   end
   
+# the below is a DELETE request that destroys a specific todo based on the ID
+
 delete '/todo/:id' do
     begin
       id = params[:id]
